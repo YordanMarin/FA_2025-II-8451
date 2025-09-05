8eb4656723b13d034c3b3326e4529baa4faa976a
yordan <yordan@gmail.com>
2025-09-05 10:13:47 -0500

    subiendo ejer2 sem3

--- SEMANA1_C#/Program.cs
+++ SEMANA1_C#/Program.cs
@@ -10,7 +10,7 @@ namespace SEMANA1_C_
     {
         static void Main(string[] args)
         {
-            ejer1();
+            ejer2();
             Console.ReadKey(); //realizando detenimiento de consola
         }
 
@@ -25,6 +25,11 @@ namespace SEMANA1_C_
             carrera = Console.ReadLine();
 
             Console.WriteLine($"\n{nombre}, bienvenido al curso FA de {carrera}");
+        }
+
+        static void ejer2()
+        {
+            Console.WriteLine("\"Yordan\"");
         }
     }
 }
