8184d8b4c37242624f65b1aa1c1d5b44893c36f2
yordan <yordan@gmail.com>
2025-10-10 10:31:04 -0500

    sem8

---
+++ SEMANA8_PY/ejer6.py
@@ -0,0 +1,27 @@
+dolar = 3.78
+euro = 4.20
+
+def conv_d():
+    return soles/dolar
+
+def conv_e():
+    return soles/euro
+
+while True: 
+    soles = float(input("Ingrese el monto en soles: "))
+
+    print("\n1. Convertir a dolares")
+    print("2. Convertir a euros")
+    print("3. Salir")
+
+    opc = int(input("\nIngrese una opción: "))
+
+    match opc:
+        case 1: print("\nEl monto en dolares: ", round(conv_d()))
+        case 2: print("\nEl monto en euros: ", round(conv_e()))
+        case 3: exit()
+        case _: print("\nOpción no válida.")
+
+    conti = input("\n¿Desea continuar? presione(y): ")
+    if conti != "y": break
+
